29e8865c25f979fac172e8e1745c214d0a2d2f13
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2016-02-10 06:50:17 -0600

    working on sync

--- src/Application.js
+++ src/Application.js
@@ -11,7 +11,7 @@ export default class Application {
     this.routes = properties.routes
     this.element = properties.element || document.getElementById('app')
     this.domains = properties.domains || {}
-    this.middleware = properties.middleware || []
+    this.middlewareGenerators = properties.middlewareGenerators || []
     this.reducers = {}
     for (var key in this.domains) {
       if (!t.Nil.is(this.domains[key].reducer)) {
@@ -19,7 +19,7 @@ export default class Application {
       }
     }
     if (!!this.routes) {
-      this.store = createStore(this.routes, this.domains, middleware)(combineReducers(this.reducers))
+      this.store = createStore(this.routes, this.domains, this.middlewareGenerators)(combineReducers(this.reducers))
       this.router = createRouter(this.store, this.routes)
     }
   }
--- src/createStore.js
+++ src/createStore.js
@@ -9,38 +9,38 @@ import sagaMiddleware                            from 'redux-saga'
 import domainMiddleware                          from './domainMiddleware'
 
 function sagaMiddlewareGenerator(domains){
-  let sagas = []
-  for (var domainName in domains) {
-    let sagasDict = domains[domainName].get('sagas')
-    for (var sagaName in sagasDict) {
-      sagas.push(sagasDict[sagaName])
+    let sagas = []
+    for (var domainName in domains) {
+        let sagasDict = domains[domainName].get('sagas')
+        for (var sagaName in sagasDict) {
+            sagas.push(sagasDict[sagaName])
+        }
     }
-  }
-  return sagaMiddleware(...sagas),
+    return sagaMiddleware(...sagas)
 }
 
-export default function createStore(routes, domains, middlewareGenerators=[]) {
-  return compose(
-    applyMiddleware(
-      sagaMiddlewareGenerator(domains),
-      domainMiddleware,
-      thunk,
-      ...middlewareGenerators.map(generator => generator(domains)),
-      createLogger({
-        predicate: (getState, action) =>
-        (process.env.NODE_ENV === 'development' &&
-        action['type'] !== 'EFFECT_TRIGGERED' &&
-        action['type'] !== 'EFFECT_RESOLVED' &&
-        !action['type'].startsWith("@@redux")),
-        actionTransformer: (action) => {
-          if (action['type'] === '@@reactuate/action') {
-            return action.payload
-          } else {
-            return action
-          }
-        }
-      })
+export default function (routes, domains, middlewareGenerators=[]) {
+    return compose(
+        applyMiddleware(
+            sagaMiddlewareGenerator(domains),
+            domainMiddleware,
+            thunk,
+            ...middlewareGenerators.map(generator => generator(domains)),
+            createLogger({
+                predicate: (getState, action) =>
+                (process.env.NODE_ENV === 'development' &&
+                 action['type'] !== 'EFFECT_TRIGGERED' &&
+                 action['type'] !== 'EFFECT_RESOLVED' &&
+                 !action['type'].startsWith("@@redux")),
+                actionTransformer: (action) => {
+                    if (action['type'] === '@@reactuate/action') {
+                        return action.payload
+                    } else {
+                        return action
+                    }
+                }
+            })
     ),
     reduxReactRouter({routes, createHistory})
-  )(createStore)
+    )(createStore)
 }
