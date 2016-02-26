98d888f7c1d0cafd26eb63fb9a1aea3eecd76d20
michaeljosephrosenthal <michael.the.awesome@gmail.com>
2016-02-26 11:35:41 -0600

    removing externals

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ export Domain                 from './Domain'
 export createReducer          from './createReducer'
 export createAction           from './createAction'
 export createSaga             from './createSaga'
-export React                  from 'react'
-export { Route }              from 'react-router'
-export { connect }            from 'react-redux'
-export { bindActionCreators } from 'redux'
-export t                      from 'tcomb'
+//export React                  from 'react'
+//export { Route }              from 'react-router'
+//export { connect }            from 'react-redux'
+//export { bindActionCreators } from 'redux'
+//export t                      from 'tcomb'
 
-export { take, put, race, call,
-         apply, cps, fork, join,
-         cancel, as } from 'redux-saga'
+//export { take, put, race, call,
+//         apply, cps, fork, join,
+//         cancel, as } from 'redux-saga'
